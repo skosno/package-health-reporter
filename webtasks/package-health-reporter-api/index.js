88ec6f2560050f708c4b5861ffef6838b5e6f09b
Szymon Kosno <skosno@gmail.com>
2018-02-06 18:02:24 +0100

    Use type from query params and verify versus supported repositories

--- webtasks/package-health-reporter-api/index.js
+++ webtasks/package-health-reporter-api/index.js
@@ -1,4 +1,16 @@
-module.exports = function(cb) {
+const REPOSITORY_URLS = Object.create(null, {
+  npm: { value: { type: 'npm', url: 'https://registry.npmjs.org' } },
+  git: { value: { type: 'git', url: 'https://api.github.com' } },
+});
+
+module.exports = function(ctx, cb) {
+  const typeRaw = ctx.data.type;
+
+  const repository = REPOSITORY_URLS[typeRaw];
+  if (!repository) {
+    cb(new Error('Unknown Repository'));
+  }
+
   cb(null, {
     status: 'ok',
   });
